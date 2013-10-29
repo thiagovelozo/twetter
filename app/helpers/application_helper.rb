191d38bdc4111ea4bd893ae79de2374ade8c47db
Dan Matthews <dan@bluefoc.us>
2013-10-29 12:46:51 -0400

    Merge branch 'master' into profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  # Being the ApplicationHelper, all methods herein are available in all views.
+
+  # Responsible for generating the appropriate form to follow or unfollow a user. If the
+  # authenticated user is already following the user passed, an unfollow (DELETE) form
+  # will be generated. Otherwise, a follow (CREATE) form will be generated.
+  #
   def follow_link(user, return_path = nil)
     follow = Follow.where(:user => current_user, :following => user)
     if follow.exists?
@@ -18,10 +24,16 @@ module ApplicationHelper
     end
   end
 
+  # Generates a left navigation link setting the class according to the result of a
+  # call to the #active_class method.
+  #
   def nav_item(name, path)
-    content_tag(:li, link_to(name, path), :class => is_active?(name))
+    content_tag(:li, link_to(name, path), :class => active_class(name))
   end
 
+  # Generates HTML for notices based on the flash variable. Leverages #notice_class to
+  # determine the look of each notice type.
+  #
   def notices
     content_tag :div, :class => 'alert-float' do
       html = ''
@@ -34,7 +46,10 @@ module ApplicationHelper
 
   private
 
-  def is_active?(name)
+  # Returns 'active' or '' based on the name past and the current controller. Used by
+  # #nav_item to show which page link is currently active.
+  #
+  def active_class(name)
     case name
     when "Follow" then
       controller_name == 'follows' ? 'active' : ''
@@ -43,10 +58,16 @@ module ApplicationHelper
     end
   end
 
+  # Returns true / false based on whether there is a flash notice of a specific type.
+  # Used by #notices to only display alert boxes when there is alert content.
+  #
   def notice?(type)
     flash[type].present?
   end
 
+  # Returns a css class or set of classes to be used with alerts of a specific type.
+  # Used by #notices.
+  #
   def notice_class(type)
     case type
     when :error then 'alert text-danger'
@@ -56,6 +77,8 @@ module ApplicationHelper
     end
   end
 
+  # Returns HTML for the internal content of a notice of a specific type. Used by #notices.
+  #
   def notice_text(type)
     html = content_tag :button, "Dismiss Alert", :class => 'close', 'data-dismiss' => 'alert', 'aria-hidden' => true
     html += content_tag :strong, flash[type]
