0663d2ec357f2b9424e9b5e20e8254ce177bd161
Thiago Velozo <thiagovelozo2@gmail.com>
2014-02-21 22:42:45 +0000

    commented out faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+=begin
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,3 +10,6 @@
 20.times do |number|
   user = User.create!(username: "user#{number}", email: Faker::Internet.email, name: Faker::Name.name, password: "password")
 end
+
+
+=end
