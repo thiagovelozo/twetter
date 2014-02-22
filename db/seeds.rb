b8f2a4865377bf62bd69b5892812e9e9de0d4b7a
Thiago Velozo <thiagovelozo2@gmail.com>
2014-02-22 02:02:46 +0000

    removed wrong begin end from seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-=begin
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
@@ -10,5 +10,3 @@
 20.times do |number|
   user = User.create!(username: "user#{number}", email: Faker::Internet.email, name: Faker::Name.name, password: "password")
 end
-
-=end
